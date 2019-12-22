554999a2e3dd4ddafc42c085391a57bd5ae3c27d
Meincrab <meincrab@gmail.com>
2019-12-22 23:22:21 +0200

    ...

---
+++ grokkingAlgorithms/grokkingAlgorithms/BinarySearch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace grokkingAlgorithms
+{
+    static class BinarySearch
+    {
+        public static void findValue(int[] numbers)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ grokkingAlgorithms/grokkingAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace grokkingAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
