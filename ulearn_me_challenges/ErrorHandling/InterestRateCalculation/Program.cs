699d05ccad36ea4ee758bef2bb1c7255161e975e
Meincrab <meincrab@gmail.com>
2020-08-05 14:29:45 +0300

    Part 'Debugging` done

---
+++ ulearn_me_challenges/ErrorHandling/InterestRateCalculation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Dynamic;
+
+namespace InterestRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            double userOutput = Calculate(userInput);
+
+            Console.WriteLine(userOutput);
+            Console.ReadLine();
+        }
+
+        public static double Calculate(string userInput)
+        {
+            double sum = double.Parse(userInput.Split(' ')[0]);
+            double rate = double.Parse(userInput.Split(' ')[1]) / 100;
+            int t = int.Parse(userInput.Split(' ')[2]);
+
+            for (int i = 1; i <= t; i++)
+            {
+                sum += sum * rate / 12;
+            }
+            return sum;
+        }
+    }
+}
---
+++ ulearn_me_challenges/ErrorHandling/ProjectileCalculation/AngryBirdsTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		/// <param name="v">Speed</param>
+		/// <param name="distance">Distance</param>
+		/// <returns>Aiming angle from 0 to Pi/2</returns>
+		/// 
+		public static double FindSightAngle(double v, double distance)
+		{
+			//Equation S = V^2 / G * Sin * 2a
+			double gravity = 9.8;
+			//Math.Pow can be used, but if we are looking 
+			//for power 2 / power 3 more efficient to use multiplication
+			double sin2A = distance / (v*v/gravity);
+			double angle = Math.Asin(sin2A) / 2;
+			return angle;
+		}
+	}
+}
